e6c86cf8de04f0a485c44df8acacb1c4a6dd05d2
Christian <christiansantoslima21@gmail.com>
2023-12-17 01:06:55 -0300

    build: start project

---
+++ pokedex/src/main.rs
@@ -0,0 +1,13 @@
+use yew::prelude::*;
+
+#[function_component(App)]
+fn app() -> Html {
+    html! {
+        <h1>{ "Hello World" }</h1>
+    }
+}
+
+fn main() {
+    yew::Renderer::<App>::new().render();
+}
+
