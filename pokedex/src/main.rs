148099fd429c273c832cfd3749ff48ab48d8925d
Christian <christiansantoslima21@gmail.com>
2023-12-17 05:00:46 -0300

    feat: implement html and css

--- pokedex/src/main.rs
+++ pokedex/src/main.rs
@@ -1,9 +1,27 @@
 use yew::prelude::*;
 
+
+
 #[function_component(App)]
 fn app() -> Html {
     html! {
-        <h1>{ "Hello World" }</h1>
+        <main>
+            <img src="" alt="pokemon" class="poke__image" />
+
+            <h1 class="pokemon__data">  
+                <span class="pokemon__number">{"6 - "}</span> 
+                <span class="pokemon__name">{"chalizard"}</span>
+            </h1>
+
+            <form class="form">
+                <input  
+                    type="search"
+                    class="input__search"
+                    placeholder="Name or number"
+                />
+            </form>
+            <img src="./images/pokedex.png" alt="pokedex" class="pokedex" />
+        </main>
     }
 }
 
