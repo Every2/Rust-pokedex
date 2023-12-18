26880802a5d38611091d01a8c45477a82ba6602c
Christian <christiansantoslima21@gmail.com>
2023-12-18 19:57:49 -0300

    fix: counter starts wrong

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Rust-pokedex
+
+Uma Pokedex, mas usando Yew e em Rust.
+
+# Como rodar
+
+Primeiramente configure seu target no Cargo `rustup target add wasm32-unknown-unknown` e instale o Trunk `cargo install --locked trunk` depois é só rodar `trunk serve` e irá funcionar,
--- pokedex/src/main.rs
+++ pokedex/src/main.rs
@@ -6,38 +6,41 @@ use serde_json::Value;
 #[function_component(App)]
 fn app() -> Html {
     let pokemon_state = use_state_eq::<Option<Pokemon>, _>(|| None);
-    let search_state = use_state_eq::<i32, _>(|| 1.max(1));
-    let _search_state = search_state.clone();
-    let search_state_outer = search_state.clone();
-    let _pokemon_state_outer = pokemon_state.clone();
+    let search_state = use_state_eq::<i32, _>(|| 1);
+    let clone_search_state = search_state.clone();
     let pokemon_state_outer = pokemon_state.clone();
     let onclick_prev: Callback<MouseEvent> = Callback::from(move |mouse_event| {
-        on_click(search_state_outer.clone(), pokemon_state_outer.clone(), _search_state.clone(), -1, mouse_event);
+        if clone_search_state.gt(&1) {
+            on_click(pokemon_state_outer.clone(), clone_search_state.clone(), -1, mouse_event);
+        }
     });
-    let _search_state = search_state.clone();
-    let search_state_outer = search_state.clone();
+    let clone_search_state = search_state.clone();
     let pokemon_state_outer = pokemon_state.clone();
     let onclick_next: Callback<MouseEvent> = Callback::from(move |mouse_event| {
-        on_click(search_state_outer.clone(), pokemon_state_outer.clone(), _search_state.clone(), 1, mouse_event);
+        on_click(pokemon_state_outer.clone(), clone_search_state.clone(), 1, mouse_event);
     });
-    let _search_state = search_state.clone();
+    let clone_search_state = search_state.clone();
+    let pokemon_state_outer = pokemon_state.clone();
     let onkeydown: Callback<KeyboardEvent> = Callback::from(move |keyboard_event: KeyboardEvent| {
         if keyboard_event.key() == "Enter" {
             keyboard_event.prevent_default();
             let input: HtmlInputElement = keyboard_event.target_unchecked_into();
             let value = input.value();
-            if let Ok(number) = value.parse::<i32>() {
-                _search_state.set(number);
+            if let Ok(number) = value.parse::<i32>(){
+                if number > 0 {
+                    clone_search_state.set(number)
+                }
             }
         }
     });
 
+    render_pokemon(pokemon_state_outer, search_state);
     html! {
         <main>
             <img src={format!("{}", pokemon_state.as_ref().map_or("", |p| &p.image_src))} alt="pokemon" class="poke__image" />
 
             <h1 class="pokemon__data">
-                <span class="pokemon__number">{format!("{:?} - ", search_state.to_string().parse::<i32>().unwrap())}</span>
+                <span class="pokemon__number">{format!("{} - ", pokemon_state.as_ref().map_or(0 as i32, |p| p.id))}</span>
                 <span class="pokemon__name">{format!("{}", pokemon_state.as_ref().map_or("", |p| &p.name))}</span>
             </h1>
 
@@ -57,6 +60,7 @@ fn app() -> Html {
             <img src="./images/pokedex.png" alt="pokedex" class="pokedex" />
         </main>
     }
+    
 }
 
 #[derive(Debug, PartialEq, Clone)]
@@ -67,18 +71,20 @@ struct Pokemon {
 }
 
 fn on_click(
-    search_state_outer: UseStateHandle<i32>,
     pokemon_state: UseStateHandle<Option<Pokemon>>,
     search_state: UseStateHandle<i32>,
     increment: i32,
     _mouse_event: MouseEvent,
 ) {
-    search_state.set(search_state_outer.add(increment));
-
+    search_state.set(search_state.add(increment));
+    let search_state = search_state.clone();
     let pokemon_state = pokemon_state.clone();
+    render_pokemon(pokemon_state, search_state)
+}
 
+fn render_pokemon(pokemon_state: UseStateHandle<Option<Pokemon>>, search_state: UseStateHandle<i32>) {
     wasm_bindgen_futures::spawn_local(async move {
-        let url = format!("https://pokeapi.co/api/v2/pokemon/{}", search_state_outer.to_string());
+        let url = format!("https://pokeapi.co/api/v2/pokemon/{}", search_state.to_string());
         let response = reqwest::get(url).await.unwrap();
 
         if response.status().is_success() {
